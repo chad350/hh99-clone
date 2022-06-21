766bf964a030868e04195ffe4279dbdb467ba65f
saehwa95 <100126319+saehwa95@users.noreply.github.com>
2022-06-21 20:45:30 +0900

    Add files via upload

--- airbnb/src/components/Main/Category.jsx
+++ airbnb/src/components/Main/Category.jsx
@@ -1,42 +1,71 @@
 import React from 'react'
+import styled from 'styled-components'
 import pension from '../../asset/펜션.png'
 import hotel from '../../asset/hotel.png'
 import apartment from '../../asset/아파트.png'
 import house from '../../asset/주택.png'
 import guestHouse from '../../asset/게하.png'
-import styled from 'styled-components'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { __loadPosts } from '../../redux/modules/post'
+import { GiIsland, GiCampingTent } from 'react-icons/gi'
+import { TbBeach } from 'react-icons/tb'
+
+
+
 
 const Category = () => {
+
+  // const dispatch = useDispatch();
+
+  const selectCategory = (category) => {
+    // dispatch(__loadPosts("", category));
+  }
+
   return (
     <>
       <Container>
-        <Link to={'/Main'} style={{textDecoration : 'none', color : '#717171'}}>
-          <Item style={{ display: 'flex', flexDirection: 'column' }}>
+        <Item>
+          <GiIsland style={{ width: '45px', height: '45px' }}>
+            <span>섬</span>
+          </GiIsland>
+        </Item>
+        <Item>
+          <TbBeach style={{ width: '45px', height: '45px' }}>
+            <span>해변 근처</span>
+          </TbBeach>
+        </Item>
+        <Item>
+          <GiCampingTent style={{ width: '45px', height: '45px' }}>
+            <span>캠핑장</span>
+          </GiCampingTent>
+        </Item>
+        <Link to={'/Main'} style={{ textDecoration: 'none', color: '#717171' }}>
+          <Item onClick={selectCategory("pension")}>
             <img src={pension} alt='' style={{ width: '40px', height: '40px' }} />
             <span>펜션</span>
           </Item>
         </Link>
-        <Link to={'/Main'} style={{textDecoration : 'none', color : '#717171'}}>
-          <Item style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link to={'/Main'} style={{ textDecoration: 'none', color: '#717171' }}>
+          <Item onClick={selectCategory("hotel")}>
             <img src={hotel} alt='' style={{ width: '40px', height: '40px' }} />
             <span>호텔</span>
           </Item>
         </Link>
-        <Link to={'/Main'} style={{textDecoration : 'none', color : '#717171'}}>
-          <Item style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link to={'/Main'} style={{ textDecoration: 'none', color: '#717171' }}>
+          <Item onClick={selectCategory("apartment")}>
             <img src={apartment} alt='' style={{ width: '40px', height: '40px' }} />
             <span>아파트</span>
           </Item>
         </Link>
-        <Link to={'/Main'} style={{textDecoration : 'none', color : '#717171'}}>
-          <Item style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link to={'/Main'} style={{ textDecoration: 'none', color: '#717171' }}>
+          <Item onClick={selectCategory("house")}>
             <img src={house} alt='' style={{ width: '40px', height: '40px' }} />
             <span>주택</span>
           </Item>
         </Link>
-        <Link to={'/Main'} style={{textDecoration : 'none', color : '#717171'}}>
-          <Item style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link to={'/Main'} style={{ textDecoration: 'none', color: '#717171' }}>
+          <Item onClick={selectCategory("guestHouse")}>
             <img src={guestHouse} alt='' style={{ width: '40px', height: '40px' }} />
             <span>게하</span>
           </Item>
@@ -52,13 +81,14 @@ const Container = styled.div`
 display: flex ;
 justify-content: center;
 align-content: center;
+width: 100%;
 position: fixed;
-left: 40%;
-padding-top: 40px;
 background-color: white;
 `
 
 const Item = styled.div`
+display: flex;
+flex-direction: column;
 text-align: center;
 margin-right : 40px;
 `
--- airbnb/src/redux/modules/post.jsx
+++ airbnb/src/redux/modules/post.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 // import {getCookie} from '../../shared/cookie'
 
+
 //액션타입
 const LOAD_POST = "LOAD_POST"
-const ADD_POST = "ADD_POST"
+const ADD_POST = "ADD_POST" 
+const LOAD_IMAGE = "LOAD_IMAGE"
 
 
 //액션 크리에이터
@@ -15,23 +17,47 @@ const addPost = (payload) => {
   return {type : ADD_POST, payload};
 };
 
+const loadImage = (payload) => {
+  return {type : LOAD_IMAGE, payload};
+};
+
 
 //미들웨어
-export const __loadPosts = (token) => async (dispatch, getState) => {
+  export const __loadPosts = (token) => async (dispatch, getState) => {
   try {
-    const response = await axios.get("http://15.164.50.132/api/accommodations", {
+    
+    const response = await axios.get("http://3.34.4.93/api/accommodations", {
       headers : {
-        Authorization : `Bearer ${token}`,
+        // Authorization : `Bearer ${token}`,
       }
     })
-    //31번줄 들어오는 데이터 확인 후 추가 작성 필요
     dispatch(loadPost(response.data));
   } catch (error) {
     console.log(error);
   }
 }
 
+// export const __loadPosts = (token, category) => async (dispatch, getState) => {
+//   try {
+  
+//     const params = {category : category};
+
+//     const response = await axios.get("http://15.164.50.132/api/accommodations", 
+//     {
+//       headers : {
+//         // Authorization : `Bearer ${token}`,
+//       },
+//       params : params
+//     })
+//     //31번줄 들어오는 데이터 확인 후 추가 작성 필요
+//     dispatch(loadPost(response.data));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
 export const __addPost = (payload) => async (dispatch, getState) => {
+
   // 데이터 확인을 위한 코드입니다.
   // payload.formData.forEach((form,key)=>{
   //   console.log(payload.formData.get(key));
@@ -55,10 +81,25 @@ export const __addPost = (payload) => async (dispatch, getState) => {
   }
 }
 
+export const __loadImages = () => async (dispatch, getState) => {
+  try {
+    
+    const response = await axios.get("http://3.34.4.93/image/1", {
+      headers : {
+        // Authorization : `Bearer ${token}`,
+      }
+    })
+    dispatch(loadImage(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 //초기값
 const initialState = {
   posts : [],
+  images : [],
   loading : false,
   console : null,
   detail : null,
@@ -66,7 +107,7 @@ const initialState = {
 
 //리듀서
 const postReducer = (state = initialState, action) => {
-  switch (action.payload) {
+  switch (action.type) {
     case LOAD_POST:
       console.log(action.payload);
       return { ...state, posts: action.payload };
@@ -74,6 +115,9 @@ const postReducer = (state = initialState, action) => {
     case ADD_POST:
       return { ...state, posts: [...state.posts, action.payload] };
 
+      case LOAD_IMAGE:
+        console.log(action.payload);
+        return { ...state, images: action.payload };
 
     default:
       return state;
